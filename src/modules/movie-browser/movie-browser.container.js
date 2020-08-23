8ff5ccb3e9309d8dc27baf2b6ab81160d5c07703
Nero28 <shackiy.s@yandex.ru>
2020-08-23 14:05:41 +0300

    add scroll movies

--- src/modules/movie-browser/movie-browser.container.js
+++ src/modules/movie-browser/movie-browser.container.js
@@ -5,13 +5,42 @@ import { connect } from 'react-redux';
 import * as movieActions from './movie-browser.actions';
 import * as movieHelpers from './movie-browser.helpers';
 import MovieList from './movie-list/movie-list.component';
+import * as scrollHelpers from '../common/scroll.helpers';
 
 class MovieBrowser extends React.Component {
-
-    componentDidMount() {
-        this.props.getMovies(1);
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: 1
+        };
+        // Binds the handleScroll to this class (MovieBrowser)
+        // which provides access to MovieBrowser's props
+        // Note: You don't have to do this if you call a method
+        // directly from a lifecycle method or define an arrow function
+        this.handleScroll = this.handleScroll.bind(this);
     }
 
+
+    componentDidMount() {
+        window.onscroll = this.handleScroll;
+        this.props.getMovies(this.state.currentPage);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll() {
+        const { topMovies } = this.props;
+        if (!topMovies.isLoading) {
+            let percentScrolled = scrollHelpers.getScrollDownPercentage(window);
+            if (percentScrolled > .8) {
+                const nextPage = this.state.currentPage + 1;
+                this.props.getMovies(nextPage);
+                this.setState({ currentPage: nextPage });
+            }
+        }
+    }
 
     render() {
         const { topMovies } = this.props;
@@ -25,7 +54,7 @@ class MovieBrowser extends React.Component {
                         <p>Search will go here</p>
                     </Row>
                     <Row>
-                        <MovieList movies={movies} isLoading={topMovies.isLoading}/>
+                        <MovieList movies={movies} isLoading={topMovies.isLoading} />
                     </Row>
                 </Container>
             </div>
